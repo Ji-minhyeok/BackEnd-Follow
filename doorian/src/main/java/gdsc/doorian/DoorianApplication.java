e5260e18228c5147e86d860e8b85455828492d41
Lihyonyo <may7501@naver.com>
2023-10-04 20:17:19 +0900

    initial commit

---
+++ doorian/src/main/java/gdsc/doorian/DoorianApplication.java
@@ -0,0 +1,13 @@
+package gdsc.doorian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorianApplication.class, args);
+	}
+
+}
---
+++ doorian/src/test/java/gdsc/doorian/DoorianApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.doorian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
